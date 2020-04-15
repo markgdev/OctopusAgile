3c725ee0d97d970bb23d610b8f5b65d3688b02a8
Mark Gardner <mark@familygardner.co.uk>
2020-04-15 22:06:29 +0100

    Refactored ready for PyPi packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'OctopusAgile',         # How you named your package folder (MyLib)
+  packages = ['OctopusAgile'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='Apache 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A library to interface with the Octopus Agile API',   # Give a short description about your library
+  author = 'Mark G',                   # Type in your name
+  author_email = '',      # Type in your E-Mail
+  url = 'https://github.com/markgdev/OctopusAgile',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Octopus', 'Agile'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache 2.0',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
