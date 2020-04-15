fabbe8a2576fe52afb531ddf3b8c2fe7b0f55c17
Mark Gardner <mark@familygardner.co.uk>
2020-04-15 22:13:10 +0100

    Updated licence in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'OctopusAgile',         # How you named your package folder (MyLib)
   packages = ['OctopusAgile'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='Apache 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library to interface with the Octopus Agile API',   # Give a short description about your library
   author = 'Mark G',                   # Type in your name
   author_email = '',      # Type in your E-Mail
   url = 'https://github.com/markgdev/OctopusAgile',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/markgdev/OctopusAgile/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['Octopus', 'Agile'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
