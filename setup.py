e03324d14f18afe3a1d14bbbfa8282fda082b5da
Mark Gardner <mark@familygardner.co.uk>
2020-04-15 23:04:28 +0100

    v0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name = 'OctopusAgile',         # How you named your package folder (MyLib)
   packages = ['OctopusAgile'],   # Chose the same as "name"
-  version = '0.0.4',      # Start with a small number and increase it with every change you make
+  version = '0.0.5',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library to interface with the Octopus Agile API',   # Give a short description about your library
   long_description=long_description,
@@ -15,7 +15,7 @@ setuptools.setup(
   author = 'Mark G',                   # Type in your name
   author_email = '',      # Type in your E-Mail
   url = 'https://github.com/markgdev/OctopusAgile',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/markgdev/OctopusAgile/archive/v0.0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/markgdev/OctopusAgile/archive/v0.0.5.tar.gz',    # I explain this later on
   keywords = ['Octopus', 'Agile'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'datetime',
