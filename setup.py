a1edfcd9307add38433f52bbc844980c0ca9b8d9
Mark Gardner <mark@familygardner.co.uk>
2020-04-15 22:26:34 +0100

    Working out pypi description

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
   name = 'OctopusAgile',         # How you named your package folder (MyLib)
   packages = ['OctopusAgile'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library to interface with the Octopus Agile API',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Mark G',                   # Type in your name
   author_email = '',      # Type in your E-Mail
   url = 'https://github.com/markgdev/OctopusAgile',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/markgdev/OctopusAgile/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/markgdev/OctopusAgile/archive/v0.0.1.tar.gz',    # I explain this later on
   keywords = ['Octopus', 'Agile'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
@@ -18,7 +25,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache 2.0',   # Again, pick a license
+    'License :: OSI Approved :: Apache Software License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
